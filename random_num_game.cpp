47f277ffc102e5998ec0b1ace093d6d9238ecb5c
KRISHNA-BIHARI <94449960+KRISHNA-BIHARI@users.noreply.github.com>
2023-11-09 20:31:12 +0530

    Add files via upload

---
+++ random_num_game.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    std::cout << "Enter guess limit: ";
+    int lim; std::cin >> lim;
+    srand( time(0) ^ clock() );
+    const int num = rand() % lim;
+    while (true) {
+        std::cout << "Enter your guess: ";
+        int n; std::cin >> n;
+        if (n > num && ((n-num)<=5 && (n-num)>0)) {
+            std::cout << "You are so close, go with the smaller value!\n";
+            continue;
+        }else  if (n < num && ((n-num)>=-5 && (n-num)<0)) {
+            std::cout << "You are so close, go with larger value!\n";
+            continue;
+        }else if (n < num) {
+            std::cout << "Your guess is too small\n";
+            continue;
+        }else if (n > num) {
+            std::cout << "Your guess is too large\n";
+            continue;
+        } else {
+            std::cout << "You Won!\n";
+            break;
+        }
+    }
+    
+
+    return 0;
+}
